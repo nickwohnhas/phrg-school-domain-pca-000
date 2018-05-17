2812b15b1d876b00d2a44597ecb4b432ee614aeb
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-17 16:01:16 -0400

    Practiced building objects

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+  attr_accessor :name
+  def roster=(roster)
+    @roster = roster
+
+  end
+  def roster
+    @roster
+  end
+  def add_student(student_name,grade)
+    if @roster[grade] === nil
+      @roster[grade] = []
+      @roster[grade] << student_name
+    else
+    @roster[grade] << student_name
+   end
+ end
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+  def sort
+    @roster.each do |key,value|
+      value.sort!
+
+
+    end
+  end
+end
